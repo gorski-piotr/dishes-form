1ba433c4f61d490df6b34d987cfce1277ef36215
Piotr Gorski <gorskipiotr5@gmail.com>
2021-06-04 00:04:02 +0100

    select input (type) created

--- src/components/DishesForm.js
+++ src/components/DishesForm.js
@@ -3,8 +3,10 @@ import { useState } from "react";
 function DishesForm() {
   const [name, setName] = useState("");
   const [preparation_time, setPreparation_time] = useState("00:00:00");
+  const [type, setType] = useState("");
   console.log("name:", name);
   console.log("", preparation_time);
+  console.log("type:", type);
 
   return (
     <div className="p-10 bg-yellow-500">
@@ -29,6 +31,14 @@ function DishesForm() {
         onChange={(e) => setPreparation_time(e.target.value)}
         required
       />
+      <br />
+      <label htmlFor="type">Dish type:</label>
+      <select name="type" id="type" onChange={(e) => setType(e.target.value)}>
+        <option value="">Choose dish type</option>
+        <option value="pizza">Pizza</option>
+        <option value="soup">Soup</option>
+        <option value="sandwich">Sandwich</option>
+      </select>
     </div>
   );
 }
