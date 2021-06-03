58f729d997cda9c52e3c1fc6c8255a572da9c6c6
Piotr Gorski <gorskipiotr5@gmail.com>
2021-06-03 23:25:33 +0100

    text input (dishname) created

--- src/components/DishesForm.js
+++ src/components/DishesForm.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 function DishesForm() {
+  const [dishname, setDishname] = useState("");
+  console.log(dishname);
+
   return (
     <div className="p-10 bg-yellow-500">
-      Dishes Form is going to be build here
+      <label htmlFor="dishname">Dish name: </label>
+      <input
+        type="text"
+        name="dishname"
+        id="dishname"
+        placeholder="Dish name"
+        value={dishname}
+        onChange={(e) => setDishname(e.target.value)}
+        required
+      />
     </div>
   );
 }
