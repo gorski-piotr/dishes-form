4b013d5a444358543ce09c9f9b6c5ca7e941a56b
Piotr Gorski <gorskipiotr5@gmail.com>
2021-06-03 23:52:22 +0100

    time input (preparation_time) created

--- src/components/DishesForm.js
+++ src/components/DishesForm.js
@@ -1,19 +1,32 @@
 import { useState } from "react";
 
 function DishesForm() {
-  const [dishname, setDishname] = useState("");
-  console.log(dishname);
+  const [name, setName] = useState("");
+  const [preparation_time, setPreparation_time] = useState("00:00:00");
+  console.log("name:", name);
+  console.log("", preparation_time);
 
   return (
     <div className="p-10 bg-yellow-500">
-      <label htmlFor="dishname">Dish name: </label>
+      <label htmlFor="name">Dish name: </label>
       <input
         type="text"
-        name="dishname"
-        id="dishname"
+        name="name"
+        id="name"
         placeholder="Dish name"
-        value={dishname}
-        onChange={(e) => setDishname(e.target.value)}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+      <br />
+      <label htmlFor="preparation_time">Preparation time: </label>
+      <input
+        type="time"
+        step="1"
+        name="preparation_time"
+        id="preparation_time"
+        value={preparation_time}
+        onChange={(e) => setPreparation_time(e.target.value)}
         required
       />
     </div>
